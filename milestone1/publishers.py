ba59508b72c5fb43541a7eb4cee55fd029b2ba31
wally1002 <srpr@kth.se>
2025-03-26 14:24:06 +0100

    added path publisher for after RRT

--- milestone1/publishers.py
+++ milestone1/publishers.py
@@ -3,6 +3,7 @@ import utm
 
 from sensor_msgs.msg import NavSatFix, Imu
 from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Path
 
 
 class RobotPosePublisher:
@@ -39,11 +40,18 @@ def convert_gnss_to_utm(lat, lon):
     return x, y
 
 
-if __name__ == "__main__":
-    rospy.init_node('robot_pose_publisher')
-    geo_offset = [-333520.64199354, -6582420.00142414, 0.0]
-    pose_pub = RobotPosePublisher(geo_offset)
-    rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        pose_pub.publish_robot_pose()
-        rate.sleep()
+class PathPublisher:
+
+    def __init__(self):
+        self.path = rospy.Publisher('/path', Path, queue_size=10)
+
+    def publish_path(self, path):
+        path_msg = Path()
+        path_msg.header.stamp = rospy.Time.now()
+        for point in path:
+            pose = PoseStamped()
+            pose.pose.position.x = point[0]
+            pose.pose.position.y = point[1]
+            pose.pose.orientation.z = point[2]
+            path_msg.poses.append(pose)
+        self.path.publish(path_msg)
