4a078f87aa51fd512b247287b79d40596c912d86
Sarvesh-kth <sarvesh@kth.se>
2025-04-20 14:26:27 +0200

    MultipleGoals

---
+++ sendgoal1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Pose, PoseArray
+
+def publish_goal_list():
+    rospy.init_node('goal_publisher')
+    pub = rospy.Publisher('/goal_pose', PoseArray, queue_size=10)
+    rate = rospy.Rate(0.2)  
+
+    
+    goals = PoseArray()
+    goals.header.frame_id = "map"
+
+    goal_positions = [
+        (130.0, 75.0),
+        (132.0, 76.0),
+        (134.0, 78.0)
+    ]
+
+    for pos in goal_positions:
+        pose = Pose()
+        pose.position.x = pos[0]
+        pose.position.y = pos[1]
+        pose.orientation.w = 1.0  
+        goals.poses.append(pose)
+
+    while not rospy.is_shutdown():
+        goals.header.stamp = rospy.Time.now()
+        pub.publish(goals)
+        rospy.loginfo("Published %d goals", len(goals.poses))
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        publish_goal_list()
+    except rospy.ROSInterruptException:
+        pass
